ae49d9aec3d428646b58238680c3c2ddd91d5aa6
xingxingmofash <xingxingmofashu@outlook.com>
2026-04-02 11:39:51 +0800

    fix: update repl output

--- packages/openface/src/cli/utils/createRepl.ts
+++ packages/openface/src/cli/utils/createRepl.ts
@@ -8,7 +8,7 @@ export interface CreateReplOptions extends ReadLineOptions {
 
 export async function createRepl(options: CreateReplOptions, callback?: (input: string) => Promise<string | undefined>) {
   const repl = createInterface(options)
-  repl.write(`Type \x1b[36m.copy [code]\x1b[0m to copy to clipboard. \x1b[36m.help\x1b[0m for more info.\n`)
+  console.log(`Type \x1b[36m.copy [code]\x1b[0m to copy to clipboard. \x1b[36m.help\x1b[0m for more info.\n`)
   while (true) {
     const input = await repl.question("> ")
     if (input === ".exit") {
@@ -17,17 +17,17 @@ export async function createRepl(options: CreateReplOptions, callback?: (input: 
 
     if (input === ".help") {
       const helpinfo = [{
-        titile: `\n\x1b[1mREPL Commands:\x1b[0m\n`,
+        titile: `\n\x1b[1mREPL Commands:\x1b[0m`,
         content: [
-          `  \x1b[36m.exit        \x1b[0m - Exit the REPL\n`,
-          `  \x1b[36m.copy        \x1b[0m - Copy result to clipboard (.copy [expr])\n`,
+          `  \x1b[36m.exit        \x1b[0m - Exit the REPL`,
+          `  \x1b[36m.copy        \x1b[0m - Copy result to clipboard (.copy [expr])`,
           `  \x1b[36m.help        \x1b[0m - Print this help message\n`
         ]
       }]
       helpinfo.forEach(x => {
-        repl.write(x.titile)
+        console.log(x.titile)
         x.content.forEach(c => {
-          repl.write(c)
+          console.log(c)
         })
       })
       continue
@@ -35,17 +35,17 @@ export async function createRepl(options: CreateReplOptions, callback?: (input: 
 
     const output = await callback?.(input.replace('.copy ', ''))
     if (!options.stream && output) {
-      repl.write(`${output}\n`)
+      console.log(`${output}`)
     }
 
     if (input.startsWith(".copy ")) {
       try {
         if (output) {
           await clipboardy.write(output)
-          repl.write(`\x1b[90mCopied ${output.length} characters to clipboard\x1b[0m\n`)
+          console.log(`\x1b[90mCopied ${output.length} characters to clipboard\x1b[0m`)
         }
       } catch (error) {
-        consola.error(`Failed to copy:${error}\n`)
+        consola.error(`Failed to copy:${error}`)
       }
       continue
     }
